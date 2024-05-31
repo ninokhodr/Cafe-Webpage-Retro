f73c31ffa84f5fffe5ae94f289491b3f3421311d
ninokhodr <rudie.khodr@gmail.com>
2024-05-31 16:44:54 +0200

    Different Colors on the Navbar with JS

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const juicySec = document.getElementById("juicy");
+  const cosySec = document.getElementById("cosy");
+  const navBar = document.querySelector("nav");
+
+  function updateNavColor() {
+    const juicyRect = juicySec.getBoundingClientRect();
+    const cosyRect = cosySec.getBoundingClientRect();
+    const navHeight = navBar.offsetHeight;
+
+    const isJuicyInView =
+      juicyRect.top < window.innerHeight && juicyRect.bottom > navHeight;
+
+    const isCosyInView =
+      cosyRect.top < window.innerHeight && cosyRect.bottom > navHeight;
+
+    if (isJuicyInView) {
+      navBar.classList.add("juicy");
+      navBar.classList.remove("cosy");
+    } else if (isCosyInView) {
+      navBar.classList.add("cosy");
+      navBar.classList.remove("juicy");
+    } else {
+      navBar.classList.remove("juicy");
+      navBar.classList.remove("cosy");
+    }
+  }
+
+  window.addEventListener("scroll", updateNavColor);
+
+  updateNavColor();
+});
